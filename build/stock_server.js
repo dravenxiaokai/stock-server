4a674f6adb18e12c5120d1e9746ec02d19a31e06
Draven <jspankai@vip.qq.com>
2017-08-07 13:35:22 +0800

    股票信息管理系统

---
+++ build/stock_server.js
@@ -0,0 +1,59 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var path = require("path");
+var ws_1 = require("ws");
+var app = express();
+var nodeport = 3003;
+app.use('/', express.static(path.join(__dirname, '..', 'client')));
+app.get('/api/stock', function (req, res) {
+    var result = stocks;
+    var params = req.query;
+    if (params.name) {
+        result = result.filter(function (stock) { return stock.name.indexOf(params.name) != -1; });
+    }
+    res.json(result);
+});
+app.get('/api/stock/:id', function (req, res) {
+    res.json(stocks.find(function (stock) { return stock.id == req.params.id; }));
+});
+var server = app.listen(nodeport, 'localhost', function () {
+    console.log('started on port: ' + nodeport);
+});
+var subscriptions = new Set();
+var wsServer = new ws_1.Server({ port: 8085 });
+wsServer.on('connection', function (websocket) {
+    subscriptions.add(websocket);
+});
+var messageCount = 0;
+setInterval(function () {
+    subscriptions.forEach(function (ws) {
+        if (ws.readyState === 1) {
+            ws.send(JSON.stringify({ messageCount: messageCount++ }));
+        }
+        else {
+            subscriptions.delete(ws);
+        }
+    });
+}, 2000);
+var Stock = (function () {
+    function Stock(id, name, price, rating, desc, categories) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+        this.desc = desc;
+        this.categories = categories;
+    }
+    return Stock;
+}());
+exports.Stock = Stock;
+var stocks = [
+    new Stock(1, '第一只股票', 1.99, 3.5, '这是第一只股票', ['IT', '互联网']),
+    new Stock(2, '第二只股票', 2.99, 4.5, '这是第二只股票', ['金融']),
+    new Stock(3, '第三只股票', 3.99, 2.5, '这是第三只股票', ['IT']),
+    new Stock(4, '第四只股票', 4.99, 1.5, '这是第四只股票', ['互联网']),
+    new Stock(5, '第五只股票', 5.99, 3.5, '这是第五只股票', ['IT', '互联网']),
+    new Stock(6, '第六只股票', 6.99, 4.5, '这是第六只股票', ['互联网']),
+    new Stock(7, '第七只股票', 7.99, 2.5, '这是第七只股票', ['金融'])
+];
---
+++ server/stock_server.ts
@@ -0,0 +1,67 @@
+import * as express from 'express'
+import * as path from 'path'
+import { Server } from 'ws'
+
+const app = express()
+const nodeport = 3003
+
+
+app.use('/', express.static(path.join(__dirname, '..', 'client')))
+
+app.get('/api/stock', (req, res) => {
+    let result = stocks
+    let params = req.query
+    if (params.name) {
+        result = result.filter(stock => stock.name.indexOf(params.name) != -1)
+    }
+    res.json(result)
+})
+
+app.get('/api/stock/:id', (req, res) => {
+    res.json(stocks.find(stock => stock.id == req.params.id))
+})
+
+const server = app.listen(nodeport, 'localhost', () => {
+    console.log('started on port: '+nodeport)
+})
+
+var subscriptions = new Set<any>()
+
+const wsServer = new Server({ port: 8085 })
+wsServer.on('connection', websocket => {
+    subscriptions.add(websocket)
+})
+
+var messageCount = 0
+
+setInterval(() => {
+    subscriptions.forEach(ws => {
+        if (ws.readyState === 1) {
+            ws.send(JSON.stringify({ messageCount: messageCount++ }))
+        } else {
+            subscriptions.delete(ws)
+        }
+    })
+}, 2000)
+
+export class Stock {
+    constructor(
+        public id: number,
+        public name: string,
+        public price: number,
+        public rating: number,
+        public desc: string,
+        public categories: Array<string>
+    ) {
+    }
+}
+
+const stocks: Stock[] = [
+    new Stock(1, '第一只股票', 1.99, 3.5, '这是第一只股票', ['IT', '互联网']),
+    new Stock(2, '第二只股票', 2.99, 4.5, '这是第二只股票', ['金融']),
+    new Stock(3, '第三只股票', 3.99, 2.5, '这是第三只股票', ['IT']),
+    new Stock(4, '第四只股票', 4.99, 1.5, '这是第四只股票', ['互联网']),
+    new Stock(5, '第五只股票', 5.99, 3.5, '这是第五只股票', ['IT', '互联网']),
+    new Stock(6, '第六只股票', 6.99, 4.5, '这是第六只股票', ['互联网']),
+    new Stock(7, '第七只股票', 7.99, 2.5, '这是第七只股票', ['金融'])
+];
